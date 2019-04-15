3a264d5b4b8de5abe4b7c543aefc15625a2ab11c
Charles Beynon <eToThePiIPower@gmail.com>
2019-04-15 16:42:00 -0400

    Install and configure Bootstrap

--- app/client/packs/application.js
+++ app/client/packs/application.js
@@ -15,4 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import Rails from 'rails-ujs';
+import 'bootstrap/dist/js/bootstrap';
+import 'stylesheets/application.scss'
+
+Rails.start();
